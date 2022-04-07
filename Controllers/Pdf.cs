1ebd2bad5fcbd0a64f7602585e6c70c7e4f71f2c
milosz <git@miloszmilewski.dev>
2022-04-07 21:21:05 +0100

    basic GET route

--- Controllers/Pdf.cs
+++ Controllers/Pdf.cs
@@ -1,15 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+
 
 namespace pdf_microservice.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/")]
     [ApiController]
     public class Pdf : ControllerBase
     {
+        [HttpGet("requestPdf")]
+        public async Task<IActionResult> CreatePdf()
+        {
+            var document = new PdfDocument();
+            var page = document.Pages.Add();
+
+            page.Canvas.DrawString(
+                $"Timestamp Report Date: {DateTime.Now.ToShortDateString()}",
+                new PdfFont(PdfFontFamily.Helvetica, 13f),
+                new PdfSolidBrush(Color.Black),
+                new PointF(10, 25)
+            );
+
+            var stream = new MemoryStream();
+            document.SaveToStream(stream);
+            stream.Position = 0;
+
+            return File(stream, "application/pdf", "report.pdf");
+        }
     }
 }
---
+++ Models/GeoArgument.cs
@@ -0,0 +1,9 @@
+namespace pdf_microservice.Models;
+
+public class GeoArgument
+{
+    public double lat;
+    public double log;
+
+    public DateTime timestamp;
+}
